85eb8937b2483cdf2f2c78f555f69584285952f4
Bejarano Matias <matiasosvaldobejarano2002@gmail.com>
2025-03-28 10:44:19 -0300

    se borro los primeros 9 ejercicios

---
+++ 01 Estructuras Secuenciales/BEJARANO_TP-01_v2.py
@@ -0,0 +1,13 @@
+# Punto numero 10
+print("Este programa calcula el promedio de tres números ingresados por el usuario.")
+print("Ingrese un primer numero: ")
+numero1 = int(input())
+print("Ingrese un segundo numero: ")
+numero2 = int(input())
+print("Ingrese un tercer numero: ")
+numero3 = int(input())
+promedio = (numero1 + numero2 + numero3) / 3
+print(f"El promedio de los numeros ingresados es: {promedio}")
+
+# Bejarano Matías Osvaldo
+# Cambio para Git   

75d2a664809635d0b0b3c0039c5e1584f908596e
Bejarano Matias <matiasosvaldobejarano2002@gmail.com>
2025-03-28 17:02:14 -0300

    Prueba de git 2

--- 02 Trabajo Colaborativo/pruebaGit1.py
+++ 02 Trabajo Colaborativo/pruebaGit1.py
@@ -1,4 +1,6 @@
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
 edad = 22
-print (f"Hola, soy {nombre} {apellido}, tengo {edad} años y vivo en JUJUY.")
+segundo_nombre = input("Ingrese su segundo nombre: ")
+segundo_apellido = input("Ingrese su segundo apellido: ")
+print (f"Hola, soy {nombre} {apellido} y {segundo_nombre} {segundo_apellido}, tengo {edad} años y vivo en JUJUY.")

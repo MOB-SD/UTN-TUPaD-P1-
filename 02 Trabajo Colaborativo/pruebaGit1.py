b6a74217704ef825a78213c3df91514f45854861
Bejarano Matias <matiasosvaldobejarano2002@gmail.com>
2025-04-04 16:23:59 -0300

    Se Agrego sencilles a la pagina

--- 02 Trabajo Colaborativo/pruebaGit1.py
+++ 02 Trabajo Colaborativo/pruebaGit1.py
@@ -1,6 +1,8 @@
-nombre = input("Ingrese su nombre: ")
-apellido = input("Ingrese su apellido: ")
+nombre = "Matías"
+# Se agrega un primer nombre
+apellido = "Bejarano"
+# Se agrega un primer apellido
 edad = 22
 segundo_nombre = input("Ingrese su segundo nombre: ")
-segundo_apellido = input("Ingrese su segundo apellido: ")
+# Se agrega un segundo nombre
 print (f"Hola, soy {nombre} {apellido} y {segundo_nombre} {segundo_apellido}, tengo {edad} años y vivo en JUJUY.")

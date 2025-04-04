525921a46e87dc6be44b50c25dca9e508b703c2f
Bejarano Matias <matiasosvaldobejarano2002@gmail.com>
2025-04-04 16:37:38 -0300

    Se Agrego un archiv en la rama nueva

--- 02 Trabajo Colaborativo/pruebaGit2.py
+++ 02 Trabajo Colaborativo/pruebaGit2.py
@@ -1,8 +1,6 @@
-nombre = "Matías"
-# Se agrega un primer nombre
-apellido = "Bejarano"
-# Se agrega un primer apellido
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
 edad = 22
 segundo_nombre = input("Ingrese su segundo nombre: ")
-# Se agrega un segundo nombre
+segundo_apellido = input("Ingrese su segundo apellido: ")
 print (f"Hola, soy {nombre} {apellido} y {segundo_nombre} {segundo_apellido}, tengo {edad} años y vivo en JUJUY.")
